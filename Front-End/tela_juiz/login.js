3a2668a79db0d567ab50cfd73ef2c51015ac19b4
Vinicius Ribeiro <viniciuscordeiroribeiro@gmail.com>
2024-05-05 19:25:21 -0300

    upp

--- Front-End/tela_juiz/login.js
+++ Front-End/tela_juiz/login.js
@@ -1,20 +1,17 @@
-document
-  .getElementById('loginForm')
-  .addEventListener('submit', function (event) {
-    event.preventDefault() // Evita que o formulário seja enviado normalmente
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que o formulário seja enviado normalmente
 
-    // Obter os valores de email e senha
-    var email = document.getElementById('email').value
-    var senha = document.getElementById('senha').value
+  // Obter os valores de email e senha
+  var email = document.getElementById("email").value;
+  var senha = document.getElementById("senha").value;
 
-    // Verificar se o email e a senha correspondem aos credenciais desejados
-    if (email === 'contadojuiz1359@gmail.com' && senha === 'soeuseiasenha') {
+  // Verificar se o email e a senha correspondem aos credenciais desejados
+  if (email === "contadojuiz1359@gmail.com" && senha === "soeuseiasenha") {
       // Redirecionar para a página de destino após o login bem-sucedido
-      window.location.href = 'home.html'
-    } else {
+      window.location.href = "home.html";
+  } else {
       // Exibir mensagem de erro se as credenciais estiverem incorretas
-      var errorMessage = document.getElementById('error-message')
-      errorMessage.innerHTML =
-        'Credenciais inválidas. Por favor, tente novamente.'
-    }
-  })
+      var errorMessage = document.getElementById("error-message");
+      errorMessage.innerHTML = "Credenciais inválidas. Por favor, tente novamente.";
+  }
+});
